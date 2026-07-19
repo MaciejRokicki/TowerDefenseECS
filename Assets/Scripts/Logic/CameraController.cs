45100bf863f5e8f1e66f60af6cbfb137afda41d1
MaciejRokicki <rokicki.mk@gmail.com>
2026-07-19 18:57:33 +0200

    camera controller adjustments

--- Assets/Scripts/Logic/CameraController.cs
+++ Assets/Scripts/Logic/CameraController.cs
@@ -9,11 +9,19 @@ namespace TD.Logic
         [SerializeField]
         private Camera camera;
 
-        [Header("Settings")]
+        [Header("Settings - Movement")]
         [SerializeField]
-        private float movementSpeed;
+        private AnimationCurve movementSpeedCurve;
         [SerializeField]
-        private float swipeMovementSpeed;
+        private AnimationCurve swipeMovementSpeedCurve;
+        [SerializeField]
+        private Vector2 blCorner;
+        [SerializeField]
+        private Vector2 trCorner;
+        [Header("Settings - Zoom")]
+        [SerializeField]
+        [Range(0.0f, 1.0f)]
+        private float startZoom;
         [SerializeField]
         private float zoomSpeed;
         [SerializeField]
@@ -22,26 +30,59 @@ namespace TD.Logic
         private float maxZoom;
 
         private Vector3 targetPosition;
-        private float targetCameraSize;
+        private float targetZoom;
+        private float zoomPercentage;
+
+        private Vector2 blViewport;
+        private Vector2 trViewport;
+
+        private void Awake()
+        {
+            camera.orthographicSize = targetZoom = startZoom * (maxZoom - minZoom) + minZoom;
+        }
 
         private void Update()
         {
             if (GameplayInputActionMap.IsSwiping)
             {
-                targetPosition += GameplayInputActionMap.SwipeMovement * swipeMovementSpeed * Time.deltaTime;
+                targetPosition -= GameplayInputActionMap.SwipeMovement * swipeMovementSpeedCurve.Evaluate(zoomPercentage) * Time.deltaTime;
             }
             else
             {
-                targetPosition += GameplayInputActionMap.Movement * movementSpeed * Time.deltaTime;
+                targetPosition += GameplayInputActionMap.Movement * movementSpeedCurve.Evaluate(zoomPercentage) * Time.deltaTime;
             }
 
             targetPosition.z = -100.0f;
 
-            targetCameraSize += GameplayInputActionMap.Zoom;
-            targetCameraSize = Mathf.Clamp(targetCameraSize, minZoom, maxZoom);
+            targetZoom += GameplayInputActionMap.Zoom;
+            targetZoom = Mathf.Clamp(targetZoom, minZoom, maxZoom);
 
-            camera.transform.position = Vector3.Lerp(camera.transform.position, targetPosition, movementSpeed * Time.deltaTime);
-            camera.orthographicSize = Mathf.Lerp(camera.orthographicSize, targetCameraSize, zoomSpeed * Time.deltaTime);
+            targetPosition = ClampCamera(targetPosition);
+
+            camera.transform.position = Vector3.Lerp(camera.transform.position, targetPosition, movementSpeedCurve.Evaluate(zoomPercentage) * Time.deltaTime);
+            camera.orthographicSize = Mathf.Lerp(camera.orthographicSize, targetZoom, zoomSpeed * Time.deltaTime);
+            zoomPercentage = (targetZoom - minZoom) / (maxZoom - minZoom);
+        }
+
+        private Vector3 ClampCamera(Vector3 targetPosition)
+        {
+            float width = camera.orthographicSize * camera.aspect;
+            blViewport = trViewport = camera.transform.position;
+            blViewport.x -= width;
+            blViewport.y -= camera.orthographicSize;
+            trViewport.x += width;
+            trViewport.y += camera.orthographicSize;
+
+            targetPosition.x = Mathf.Clamp(targetPosition.x, blCorner.x + width, trCorner.x - width);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, blCorner.y + camera.orthographicSize, trCorner.y - camera.orthographicSize);
+
+            return targetPosition;
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawCube(blCorner, Vector3.one);
+            Gizmos.DrawCube(trCorner, Vector3.one);
         }
     }
 }
