ab87d19b5bcec7f7252aa0fe7b7d1119df14c535
MaciejRokicki <rokicki.mk@gmail.com>
2026-08-30 20:22:20 +0200

    added DebugUI packages

---
+++ Assets/_Project/Features/Debug/Runtime/DebugViewBuilder.cs
@@ -0,0 +1,62 @@
+using DebugUI;
+using TD.Features.Experience.Runtime.Components;
+using TD.Features.Health.Runtime.Components;
+using TD.Features.Player.Runtime.Components;
+using Unity.Entities;
+
+namespace TD.Features.Debug.Runtime
+{
+    public class DebugViewBuilder : DebugUIBuilderBase
+    {
+        private EntityManager entityManager;
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+        }
+
+        protected override void Configure(IDebugUIBuilder builder)
+        {
+            builder.ConfigureWindowOptions(options =>
+            {
+                options.Title = "Debug Window";
+            });
+
+            builder.AddTabView(builder =>
+            {
+                builder.AddTab("Health", builder =>
+                {
+                    builder.AddButton("Heal (100)", () =>
+                    {
+                        var playerSingletonQuery = entityManager.CreateEntityQuery(ComponentType.ReadOnly<PlayerSingleton>());
+
+                        if (playerSingletonQuery.HasSingleton<PlayerSingleton>())
+                        {
+                            var entity = entityManager.CreateEntity();
+                            entityManager.AddComponentData(entity, new IncreaseHealthCommand()
+                            {
+                                Entity = playerSingletonQuery.GetSingletonEntity(),
+                                Value = 100
+                            });
+                        }
+
+                        playerSingletonQuery.Dispose();
+                    });
+                });
+                builder.AddTab("Experience", builder =>
+                {
+                    builder.AddButton("Add XP (100)", () =>
+                    {
+                        var entity = entityManager.CreateEntity();
+                        entityManager.AddComponentData(entity, new IncreaseExperienceCommand()
+                        {
+                            Value = 100
+                        });
+                    });
+                });
+            });
+        }
+    }
+}
