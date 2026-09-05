2b180f4679ac0be24b54918392c3998775fbffd5
MaciejRokicki <rokicki.mk@gmail.com>
2026-09-05 15:24:27 +0200

    changed GameFlowController to PauseMenuPresenter

--- Assets/_Project/UI/PauseMenuPresenter.cs
+++ Assets/_Project/UI/PauseMenuPresenter.cs
@@ -1,12 +1,10 @@
-using TD.Application.Input.ActionMaps;
 using TD.Application.StateMachine.Overlay;
-using TD.Core.InputManager;
 using TD.UI.PauseMenu.Runtime;
 using UnityEngine;
 
 namespace TD.UI
 {
-    public class GameFlowController : MonoBehaviour
+    public class PauseMenuPresenter : MonoBehaviour
     {
         private void Start()
         {
@@ -20,28 +18,14 @@ namespace TD.UI
             PauseMenuOverlay.OnPauseMenuHide -= PauseMenuOverlay_OnPauseMenuHide;
         }
 
-        private void EnablePauseMenu()
-        {
-            Time.timeScale = 0.0f;
-            PauseMenuViewModel.Instance.Show();
-            InputManager.EnableActionMap(UI_InputActionMap.Instance);
-        }
-
-        private void DisablePauseMenu()
-        {
-            InputManager.DisableRecentActionMap();
-            PauseMenuViewModel.Instance.Hide();
-            Time.timeScale = 1.0f;
-        }
-
         private void PauseMenuOverlay_OnPauseMenuShow()
         {
-            EnablePauseMenu();
+            PauseMenuViewModel.Instance.Show();
         }
 
         private void PauseMenuOverlay_OnPauseMenuHide()
         {
-            DisablePauseMenu();
+            PauseMenuViewModel.Instance.Hide();
         }
     }
 }
